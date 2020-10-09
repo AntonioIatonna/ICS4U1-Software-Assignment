21246f046f657068dc9069bedaca207bb7e4427a
Antonio <ant.iatonna@gmail.com>
2020-10-09 15:21:20 -0400

    Further Tkinter Tests

--- Test Scripts/dropDownMenus.py
+++ Test Scripts/dropDownMenus.py
@@ -5,6 +5,7 @@ def nothing():
 
 root = Tk()
 
+#Creating the main menu
 myMenu = Menu(root)
 root.config(menu = myMenu)
 
@@ -18,5 +19,19 @@ fileMenu.add_command(label = "Exit", command = nothing)
 editMenu = Menu(myMenu)
 myMenu.add_cascade(label = "Edit", menu = editMenu)
 editMenu.add_command(label = "Redo", command = nothing)
+
+#Creating toolbar
+toolbar = Frame(root, bg = "blue")
+
+insertButton = Button(toolbar, text = "Insert Image", command = nothing)
+insertButton.pack(side = LEFT, padx = 2, pady = 2)
+printButton = Button(toolbar, text = "Print", command = nothing)
+printButton.pack(side = LEFT, padx = 2, pady = 2)
+
+toolbar.pack(side = TOP, fill = X)
+
+#Creating status bar
+status = Label(root, text = "Prepairing to do nothing...", bd = 1, relief = SUNKEN, anchor = W)
+status.pack(side = BOTTOM, fill = X)
 
 root.mainloop()
