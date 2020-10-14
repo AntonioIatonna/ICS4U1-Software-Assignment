88778e0a1393f54e4d4c28625b410c0139b4a998
Antonio <ant.iatonna@gmail.com>
2020-10-13 21:14:17 -0400

    Essential Code Completed
    TODO:
    Prevent user from entering unwanted values
    Clean code and develop into functions

--- Lottery Program/lottery.py
+++ Lottery Program/lottery.py
@@ -4,6 +4,7 @@ import random
 
 tickets = []
 cpuTicket = []
+matches = []
 
 print("Welcome to Lotto 6/49!\nPlease choose how many tickets you would like to play...")
 ticketNumber = int(input())
@@ -12,7 +13,6 @@ ticketNumber = int(input())
 tickets = [0] * ticketNumber
 for i in range(ticketNumber):
     tickets[i] = [0] * 6
-
 # configures array for cpu ticket
 cpuTicket = [0] * 6
 
@@ -22,7 +22,7 @@ for x in range(ticketNumber):
     for y in range(6):
         print("Please select number " + str(y + 1))
         tickets[x][y] = int(input())
-
+        
     #improve sort (sort as you go)
     for j in range(6):
         for k in range(j + 1, 6):
@@ -31,16 +31,15 @@ for x in range(ticketNumber):
                 tickets[x][j] = tickets[x][k]
                 tickets[x][k] = temp
 
+    print(tickets[x])
+
 # generate and sort winning ticket and check and replace duplicate numbers
 for m in range(6):
     num = random.randint(1, 49)
     for n in range(6):
-        if(num == cpuTicket[n]):
+        while(num == cpuTicket[n]):
             num = random.randint(1, 49)
-        else:
-            pass
     cpuTicket[m] = num    
-
 #improve sort (sort as you go)
 for a in range(6):
     for b in range(a + 1, 6):
@@ -50,3 +49,22 @@ for a in range(6):
             cpuTicket[b] = temp
 
 print(cpuTicket)
+
+# compare all user tickets to winning ticket
+for w in range(ticketNumber):
+    userMatch = 0
+    for z in range(6):
+        for v in range(6):
+            if(tickets[w][z] == cpuTicket[v]):
+                userMatch +=1
+            else:
+                pass
+    matches.append(userMatch)
+
+# check if user won and print result
+for u in range(ticketNumber):
+    if(matches[u] >= 3):
+        print("Congratulations! You won on ticket " + str(u + 1) + " with " + str(matches[u]) + " numbers!")
+    else:
+        print("Sorry. You did not win on ticket " + str(u + 1) + " with only " + str(matches[u]) + " numbers matching.")
+    time.sleep(2)
